e691572cdf48b8c60b190ed2c0e76c470871a6a7
puja <deshmukhpooja976@gmail.com>
2022-12-16 22:06:50 +0530

    add

--- OneClickShopping/src/main/java/com/gabby/Controller/UserController.java
+++ OneClickShopping/src/main/java/com/gabby/Controller/UserController.java
@@ -1,7 +1,8 @@
-package com.gabby.Controller;
+package com.masai.controller;
 
-
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -18,20 +19,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gabby.Exception.CustomerException;
-import com.gabby.Exception.LoginException;
-import com.gabby.Service.CustomerService;
-import com.gabby.Service.LoginService;
-import com.gabby.model.Customer;
-import com.gabby.model.Login;
+import com.masai.dto.ProductDto;
+import com.masai.exception.AddressException;
+import com.masai.exception.CartException;
+import com.masai.exception.CustomerException;
+import com.masai.exception.LoginException;
+import com.masai.exception.OrderExcetion;
+import com.masai.exception.ProductException;
+import com.masai.model.Address;
+import com.masai.model.Cart;
+import com.masai.model.Customer;
+import com.masai.model.Login;
+import com.masai.model.Orders;
+import com.masai.model.Product;
+import com.masai.service.AddressService;
+import com.masai.service.CartService;
+import com.masai.service.CustomerService;
+import com.masai.service.LoginService;
+import com.masai.service.OrderService;
+import com.masai.service.ProductService;
 
 
-
-@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
 	
@@ -39,9 +52,51 @@ public class UserController {
 	@Autowired
 	private CustomerService cus;
 	
+	
 	@Autowired
 	private LoginService login;
 	
+	@Autowired
+	private AddressService ads;
+	
+	@Autowired
+	private CartService car;
+	
+	@Autowired
+	private OrderService odd;
+	
+	@Autowired
+	private ProductService prod;
+	
+	
+	@PostMapping("/addCustomer")
+	 public ResponseEntity<Customer> addcustomer(@Valid @RequestBody Customer customer) throws  CustomerException
+	 {
+		 Customer addCustomer = cus.addCustomer(customer);
+		 
+		 return new ResponseEntity<Customer>(addCustomer, HttpStatus.CREATED);
+	 }
+	
+	
+	@PutMapping("/updacustomer/{key}")
+	public ResponseEntity<Customer> updateCustomer(@PathVariable("key") String key, @RequestBody Customer customer) throws LoginException,CustomerException
+	{
+		Customer update = cus.updateCust(customer, key);
+		
+		
+		return new ResponseEntity<Customer>(update, HttpStatus.CREATED);
+	}
+	
+	
+	
+	@DeleteMapping("/delAccount{key}")
+	public ResponseEntity<Customer> delAccount(@PathVariable("key") String key, @RequestBody Customer customer) throws LoginException, CustomerException
+	{
+		
+		Customer del = cus.removeCustomer(customer, key);
+		
+		return new ResponseEntity<Customer>(del, HttpStatus.OK);
+	}
 	
 	
 	@PostMapping("/login")
@@ -61,57 +116,134 @@ public class UserController {
 	}
 	
 	
-	
-	
-	@PostMapping("/customers")
-	 public ResponseEntity<Customer> addcustomer(@Valid @RequestBody Customer customer) throws  CustomerException
-	 {
-		 Customer addCustomer = cus.addCustomer(customer);
-		 
-		 return new ResponseEntity<Customer>(addCustomer, HttpStatus.CREATED);
-	 }
-	
-	@PutMapping("/customers")
-	public  ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer,@RequestParam(required = false) String key ) throws CustomerException, javax.security.auth.login.LoginException {
+	@PostMapping("/address")
+    public ResponseEntity<Address> addaddress(@RequestBody Address address, @RequestParam String key) throws AddressException, LoginException
+    {
+		Address s = ads.addAddress(address, key);
 		
+		return new ResponseEntity<Address>(s, HttpStatus.OK);
+    }
+	
+	@PutMapping("/address")
+    public ResponseEntity<Address> updateAdd(@RequestBody Address address, @RequestParam String key) throws AddressException, LoginException
+    {
+		Address s = ads.updateAddress(address, key);
 		
-		Customer updatedCustomer= cus.updateCust(customer, key);
-				
-		return new ResponseEntity<Customer>(updatedCustomer,HttpStatus.OK);
+		return new ResponseEntity<Address>(s, HttpStatus.OK);
+    }
+	
+	@DeleteMapping("/address{delid}")
+	public ResponseEntity<String> deladdres(@PathVariable("delid") Integer de, @RequestParam String key) throws LoginException ,CustomerException
+	{
+		String s = ads.removeAddress(de, key);
+		
+		return new ResponseEntity<String>(s, HttpStatus.OK);
+	}
+	
+	
+	
+	@PostMapping("/cadd")
+	public ResponseEntity<Cart> addtoCart(@RequestParam Integer productId, @RequestParam Integer quantity, @RequestParam String key) throws CartException,LoginException,ProductException
+	{
+		
+		Cart cartIteam = car.addProductToCart(productId, quantity, key);
+		
+		return new ResponseEntity<Cart>(cartIteam, HttpStatus.OK);
+	}
+	
+	
+	@DeleteMapping("/cart/r")
+	public ResponseEntity<List<ProductDto>> removefrom(@RequestParam Integer productid,@RequestParam  String key) throws CartException, LoginException
+	{
+		List<ProductDto> list = car.removeProductfromCart(productid, key);
+		
+		return new ResponseEntity<List<ProductDto>>(list, HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/getprod")
+	public ResponseEntity<List<ProductDto>> getallcart(@RequestParam String key) throws CartException,LoginException
+	{
+		List<ProductDto> s = car.viewAllProducts(key);
+		
+		return new ResponseEntity<List<ProductDto>>(s, HttpStatus.OK);
+	}
+	
+	@PostMapping("/orders")
+	public ResponseEntity<Orders> addOrder(@RequestBody Orders order, @RequestParam String key) throws OrderExcetion, LoginException
+	{
+		Orders added = odd.addOrder(order, key);
+		
+		return new ResponseEntity<Orders>(added,HttpStatus.OK);
+	}
+	
+	@PutMapping("/orderup")
+	public ResponseEntity<Orders> updateOrder(@RequestBody Orders order, @RequestParam String key) throws LoginException,OrderExcetion
+	{
+		
+		Orders s = odd.updateCustomer(order, key);
+		
+		return new ResponseEntity<Orders>(s,HttpStatus.OK);
 		
 	}
 	
 	
-//	@PutMapping("/updacustomer/{key}")
-//	public ResponseEntity<Customer> updateCustomer(@PathVariable("key") String key, @RequestBody Customer customer) throws LoginException,CustomerException, javax.security.auth.login.LoginException
+	
+	@DeleteMapping("/delt{orderid}")
+	public ResponseEntity<Orders> removeorder(@PathVariable("orderid") Integer orderId, @RequestParam String uuid) throws OrderExcetion, LoginException
+	{
+		Orders delt = odd.removeOrder(orderId, uuid);
+		
+		return new ResponseEntity<Orders>(delt, HttpStatus.OK);
+	}
+	
+	@GetMapping("/allorder/{date}")
+	public ResponseEntity<List<Orders>> georderbydate(@PathVariable("data") LocalDate date) throws OrderExcetion
+	{
+		
+		   List<Orders> s= odd.viewallOrderByDate(date);
+		   
+		   return new ResponseEntity<List<Orders>>(s, HttpStatus.OK);
+		   
+	}
+	
+	@GetMapping("/vieworder")
+     public ResponseEntity<Orders> viewOrder(@RequestParam Integer orderId) throws OrderExcetion
+     
+     {
+		Orders s = odd.viewOrder(orderId);
+		
+		return new ResponseEntity<Orders>(s,HttpStatus.OK);
+     }
+	
+//	@GetMapping
+//	public ResponseEntity<List<Orders>> getAllorderuserid(@RequestParam String userid) throws OrderExcetion
 //	{
-//	Customer update =cus.updateCust(customer, key);
 //		
-//		
-//		
-//		return new ResponseEntity<Customer>(update, HttpStatus.CREATED);
 //	}
 	
 	
-	
-	
-	 @GetMapping("/getdeta")
-	   public ResponseEntity<Customer> getbyid(@RequestBody Customer p)throws CustomerException{
-		   
-		   Customer f = cus.viemCustomer(p.getCustomerId());
-		   
-		   return new ResponseEntity<Customer>(f,HttpStatus.ACCEPTED);
-		   
-		   
-	   }
-	 @GetMapping("/getstudents/{location}")
-		public ResponseEntity<List<Customer>> getStudentsByAddressHandler(@PathVariable("location") String location){
-			
-			
-			List<Customer> students= cus.viewAll(location);
-			
-			
-			return new ResponseEntity<List<Customer>>(students,HttpStatus.OK);
-		}
+	@GetMapping("/allproduct")
+	public ResponseEntity<List<Product>> getallprod()throws ProductException
+	{
+		List<Product> allpr = prod.viewAllproduct();
 		
+		return new ResponseEntity<List<Product>>(allpr,HttpStatus.OK);
+	}
+	
+	
+	
+	@GetMapping("/p/c/{catname}")
+	public ResponseEntity<List<Product>> GetprodtByCat(@PathVariable("catname") String cat) throws ProductException
+	{
+		List<Product> s = prod.viewProudctbyCategory(cat);
+		
+		return new ResponseEntity<List<Product>>(s,HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
 }
